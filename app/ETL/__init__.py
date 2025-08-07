b78ecd65f01336c8192581667907dd2b4c06e9f4
Mylena Vasconcelos <mylenavasconcelos@hotmail.com>
2025-08-07 10:35:10 -0300

    limpando o template para começar o meu projeto

--- app/ETL/__init__.py
+++
@@ -1,6 +0,0 @@
-"""This module contains functions for the ETL process."""
-
-from .extract import extract_excel
-from .load import load_em_um_novo_excel
-from .pipeline import pipeline_completa
-from .transform import transforma_em_um_unico
--- app/utils/__init__.py
+++
@@ -1,3 +0,0 @@
-"""This module contains utility functions for the application."""
-
-from .absenteeism_generator import generate_absenteeism_data
